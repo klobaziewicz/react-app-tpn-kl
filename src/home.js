5fbe7b39a336e2840280b85308e12cb3fbbd40a4
Krzysztof <klobaziewicz@wp.pl>
2022-07-31 18:06:46 +0200

    nowe pliki

---
+++ src/home.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Login from './login';
+import Footer from './footer';
+import './styles/index.scss';
+import ph from './images/ph.png'
+import hm from './images/hm.png'
+import ml from './images/ml.png'
+import React from "react";
+
+function Home() {
+    return (
+        <div className="App">
+            <div className='header'>
+                <h1>Zaloguj się i zgłoś wyjście taternickie</h1>
+            </div>
+            <div className="main">
+                <Login></Login>
+            </div>
+            <div className='aside'>
+                <h2>Kontakt</h2>
+                <div className='aside_icons'>
+                    <div className='B_icon'>
+                        <img alt="phone icon" src={ph} className='big_icon'></img>
+                        <span>+48 18 20 23 200</span>
+                    </div>
+                    <div className='B_icon'>
+                        <img alt="mail icon" src={ml} className='big_icon'></img>
+                        <span>sekretariat@tpn.pl</span>
+                    </div>
+                    <div className='B_icon'>
+                        <img alt="home icon" src={hm} className='big_icon'></img>
+                        <span><a href='https://tpn.pl/'>tpn.pl</a></span>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+
+export default Home;
---
+++ src/warunki.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, Component } from 'react';
+import { useForm } from "react-hook-form";
+import './styles/index.scss';
+import Footer from './footer';
+import Wpis from './wpis';
+
+function Warunki() {
+    let [count, setCount] = useState({ loaded: false, inf: '', data: {} });
+    const { register, handleSubmit } = useForm();
+    const description = 'aaa'
+    const regex1=/T/i;
+    const regex2=/Z/i;
+
+    useEffect(() => {
+        //console.log(count.inf);
+        select_qr()
+    })
+
+    const select_qr = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/select")
+            const jsonData1 = await response.json()
+            //sortuję
+            let jsonData = [...jsonData1].sort((a, b) => b.id - a.id);
+            console.log(jsonData);
+
+            //sprawdzam, czy data jest aktualna
+            if (Object.keys(jsonData).length != Object.keys(count.data).length)
+                setCount(count = { inf: count.inf, loaded: true, data: jsonData })
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const handleInfo = (data) => {
+        zmiana(data.info)
+        console.log(data.info);
+    }
+
+    let zmiana = async (a) => {
+        setCount(count = { loaded: false, inf: a, data: {} })
+        //
+        try {
+            const response = await fetch("http://localhost:5000/add/?info="+a)
+            const jsonData1 = await response.json()
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
+    return (
+        <div>
+            <div className='container_w'>
+                <h1>Warunki wspinaczkowe</h1>
+                <form className='form2' onSubmit={handleSubmit(handleInfo)}>
+                    <input placeholder="Napisz informację o warunkach" type="text" name="info" {...register('info')} required /><br></br>
+                    <input type="submit" className='btn2' value="Dodaj informację" />
+                </form>
+                <div className='info'>
+                    <div className='cont_info'>
+                        {count.loaded ? 
+                        Object.keys(count.data).map(key => (
+                            <Wpis key={key} id={count.data[key].id} info={count.data[key].info} data={count.data[key].data.replace(regex1, ' ').replace(regex2, '')}/>
+                        ))
+                        : <div>brak informacji/nie wczytano informacji</div>}
+                    </div>
+                </div>
+                <Footer></Footer>
+            </div>
+        </div>
+    );
+}
+
+
+export default Warunki;
