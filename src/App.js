dd9bfbd1a8719bb18f1cde7001b8bc137a06b8ea
Krzysztof <klobaziewicz@wp.pl>
2022-07-31 17:48:59 +0200

    wczytanie react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './home';
+import Warunki from './warunki';
+import Register from './register';
+import Admin from './admin';
+import './styles/index.scss';
+import topr from './images/topr.png'
+import tpn from './images/tpn.png'
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Router>
+
+        <div className='nav'>
+          <div className='images'>
+            <img alt='tpn image' src={topr}></img>
+            <img alt='topr image' src={tpn}></img>
+          </div>
+
+          <ul>
+            <li>
+              <Link to="/react-app-tpn-kl/">Home</Link>
+            </li>
+            <li>
+              <Link to="/react-app-tpn-kl/warunki">Warunki</Link>
+            </li>
+            <li>
+              <Link to="/react-app-tpn-kl/register">Rejestracja</Link>
+            </li>
+          </ul>
+
+        </div>
+
+        <Routes>
+          <Route path="/react-app-tpn-kl/" element={<Home />} />
+          <Route path="/react-app-tpn-kl/warunki" element={<Warunki />} />
+          <Route path="/react-app-tpn-kl/register" element={<Register />} />
+          <Route path="/react-app-tpn-kl/admin" element={<Admin />} />
+        </Routes>
+
+      </Router>
+
     </div>
   );
 }
